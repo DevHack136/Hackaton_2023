62455cd301ca75f45055ebd3003564c350063a62
__DevHack__ <trystan.legrand-simon@epitech.eu>
2023-05-22 14:46:37 +0200

    BASE

---
+++ mdpa_2.0/src/components/Navbar.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { ArrowRightIcon } from "@heroicons/react/solid";
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <header className="">
+            <div className="">
+                <div className="">
+                    <a href="#accueil" className="">
+                        Accueil
+                    </a>
+                </div>
+                <nav className="">
+                    <a href="#GHINS" className="">
+                        GHINS
+                    </a>
+                    <br />
+                    <a href="#Gouvernance" className="">
+                        Gouvernance
+                    </a>
+                    <br />
+                    <a href="#RGPD" className="">
+                        RGPD
+                    </a>
+                </nav>
+                <a
+                    href="#contact"
+                    className="">
+                    Contactez-nous !
+                    <ArrowRightIcon className="" />
+                </a>
+            </div>
+        </header>
+    );
+}
---
+++ mdpa_2.0/tailwind.config.js
@@ -0,0 +1,13 @@
+/* Import des styles de base, des composants et des utilitaires */
+
+
+/* Configuration de Tailwind CSS */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
